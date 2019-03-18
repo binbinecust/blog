385948642b53ac5ec304e4a07e0a0997943adba0
binbinfang <binbinfang@sohu-inc.com>
2019-03-18 16:38:32 +0800

    update 删除userid的唯一性

--- server/models/daily.js
+++ server/models/daily.js
@@ -1,12 +1,11 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const DailySchema = new Schema(
   {
     userId: {
       type: String,
-      required: true,
-      unique: true
+      required: true
     },
     content: {
       type: String,
@@ -14,6 +13,6 @@ const DailySchema = new Schema(
     }
   },
   { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } }
-)
+);
 
-module.exports = mongoose.model('Daily', DailySchema)
+module.exports = mongoose.model('Daily', DailySchema);
