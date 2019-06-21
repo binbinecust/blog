efa9d514b5a30885bba25b2f10af2d54ad5c31ac
方彬彬 <fangbinbin@fangbinbindeMacBook-Pro.local>
2019-06-21 10:17:24 +0800

    update 更改配置

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,6 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const CompressionWebpackPlugin = require('compression-webpack-plugin');
-const productionGzipExtensions = ['js', 'css'];
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+// const CompressionWebpackPlugin = require('compression-webpack-plugin');
+// const productionGzipExtensions = ['js', 'css'];
 module.exports = {
   transpileDependencies: [/\bvue-awesome\b/],
   devServer: {
@@ -27,26 +27,26 @@ module.exports = {
   configureWebpack: config => {
     config.entry.app = './src/main.ts';
     config.resolve.extensions = ['.js', '.vue', '.json', '.ts'];
-    if (process.env.NODE_ENV === 'production') {
-      config.plugins = config.plugins.concat([
-        new UglifyJsPlugin({
-          uglifyOptions: {
-            warnings: false,
-            compress: {
-              drop_debugger: true,
-              drop_console: true
-            }
-          },
-          sourceMap: false,
-          parallel: true
-        }),
-        new CompressionWebpackPlugin({
-          algorithm: 'gzip',
-          test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
-          threshold: 10240,
-          minRatio: 0.8
-        })
-      ]);
-    }
+    // if (process.env.NODE_ENV === 'production') {
+    //   config.plugins = config.plugins.concat([
+    //     new UglifyJsPlugin({
+    //       uglifyOptions: {
+    //         warnings: false,
+    //         compress: {
+    //           drop_debugger: true,
+    //           drop_console: true
+    //         }
+    //       },
+    //       sourceMap: false,
+    //       parallel: true
+    //     }),
+    //     new CompressionWebpackPlugin({
+    //       algorithm: 'gzip',
+    //       test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+    //       threshold: 10240,
+    //       minRatio: 0.8
+    //     })
+    //   ]);
+    // }
   }
 };
